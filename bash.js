4a8316a38aa28d6a512684640d6f458c133d7287
Trevor <take1seet@gmail.com>
2019-11-11 13:38:35 -0500

    empty pwd.js

--- bash.js
+++ bash.js
@@ -1,7 +1,9 @@
 process.stdout.write('prompt > ')
 
 process.stdin.on('data', (data) => {
+    
     const cmd = data.toString().trim();
+
 
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
